1aefa2b287dab938d3a1cea67205894911e2c4e5
yitong <yitong0768@gmail.com>
2024-12-17 22:25:46 +0800

    feat: loop

---
+++ src/04Loop/loop.py
@@ -0,0 +1,19 @@
+i = 1
+total = 0
+
+while i <= 10:
+    total += i
+    i = i + 1
+
+print(i)
+print(total)
+
+
+students = ["huang", "li", "wang"]
+for student in students:
+    print(student)
+    print("next")
+
+print("end")
+
+
--- src/04Loop/loop_turtle.py
+++ src/04Loop/loop_turtle.py
@@ -7,14 +7,20 @@ angle = 90
 
 turtle.speed(turtleSpeed)
 
-turtle.forward(distance)
-turtle.left(angle)
-turtle.forward(distance)
-turtle.left(angle)
-turtle.forward(distance)
-turtle.left(angle)
-turtle.forward(distance)
-turtle.left(angle)
+# turtle.forward(distance)
+# turtle.left(angle)
+# turtle.forward(distance)
+# turtle.left(angle)
+# turtle.forward(distance)
+# turtle.left(angle)
+# turtle.forward(distance)
+# turtle.left(angle)
+
+i = 0
+while i < 4:
+    turtle.forward(distance)
+    turtle.left(angle)
+    i += 1  # i = i + 1
 
 
 turtle.mainloop()
